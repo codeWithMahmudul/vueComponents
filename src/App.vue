b2d84e9c6fa469d81443f98496261978976380ab
MdMahmudulHasanGit <mahmudul209307@gmail.com>
2023-12-23 10:46:04 +0600

    Teleport Component

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,24 @@
 <template>
-  <button @click="activeTab = 'TabA'">Tab A</button>
-  <button @click="activeTab = 'TabB'">Tab B</button>
-  <button @click="activeTab = 'TabC'">Tab C</button>
-  <keep-alive>
-    <component :is="activeTab" />
-  </keep-alive>f
+  <teleport to="#portal-root">
+    <Portal />
+  </teleport>
 </template>
 
 <script>
-import TabA from "./components/TabA.vue";
-import TabB from "./components/TabB.vue";
-import TabC from "./components/TabC.vue";
+import Portal from "./components/Portal.vue";
 
 export default {
   name: "App",
   components: {
-    TabA,
-    TabB,
-    TabC,
+    Portal,
   },
   data() {
-    return {
-      activeTab: "TabA",
-    };
+    return {};
   },
 };
 </script>
 
-<style scoped>
+<style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
